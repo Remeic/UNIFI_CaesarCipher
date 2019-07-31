0e5ee8e33d7f7c52d862acc3a90201d41327251c
Giulio Fagioli <fagioli.giulio@gmail.com>
2019-07-31 11:26:34 +0200

    Add javadoc

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"p":"<Unnamed>","l":"AbstractCifrarium"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Cifrarium"},{"p":"<Unnamed>","l":"CifrariumOperation"},{"p":"<Unnamed>","l":"Encryptor"}]
